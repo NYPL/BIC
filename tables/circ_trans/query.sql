25910ee040a43e6ee80d2b9c9f2e468294434382
emu47 <kkelly219@gmail.com>
2017-11-29 14:44:49 -0500

    [KK] Adding volume_id to sql query.

--- tables/circ_trans/query.sql
+++ tables/circ_trans/query.sql
@@ -2,11 +2,13 @@ select
     item.record_num as item_id,
     patron.record_num as patron_id,
     bib.record_num as bib_id,
+    volume.record_num as volume_id,
     circ_trans.*
     from sierra_view.circ_trans as circ_trans
     left join sierra_view.record_metadata as item on item.id = circ_trans.item_record_id
     left join sierra_view.record_metadata as patron on patron.id = circ_trans.patron_record_id
     left join sierra_view.record_metadata as bib on bib.id = circ_trans.bib_record_id
+    left join sierra_view.volume_view as volume on volume.id = circ_trans.volume_record_id
     where circ_trans.id >= :start_id
     order by circ_trans.id
     limit :limit;
