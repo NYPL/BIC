54e0f538da4b85b00953082c4cf0100468bb528a
emu47 <kkelly219@gmail.com>
2017-11-29 15:01:00 -0500

    [KK] Reverting to use record_metadata

--- tables/circ_trans/query.sql
+++ tables/circ_trans/query.sql
@@ -8,7 +8,7 @@ select
     left join sierra_view.record_metadata as item on item.id = circ_trans.item_record_id
     left join sierra_view.record_metadata as patron on patron.id = circ_trans.patron_record_id
     left join sierra_view.record_metadata as bib on bib.id = circ_trans.bib_record_id
-    left join sierra_view.volume_view as volume on volume.id = circ_trans.volume_record_id
+    left join sierra_view.record_metadata as volume on volume.id = circ_trans.volume_record_id
     where circ_trans.id >= :start_id
     order by circ_trans.id
-    limit :limit;
+    limit 1;
